cffc500449b23329ea8d3e4136978332f2cb07ab
xiaozhou233 <xiaozhou233@vip.qq.com>
2024-08-18 03:04:53 +0800

    [+] Error handling

--- main.py
+++ main.py
@@ -3,23 +3,24 @@ from utils.WebsocketClient import WebSocketClient
 import utils.Config as Config
 
 async def main():
-    # 读取配置文件
-    ConfigHandle = Config.FileConfig("config.json")
-    bot_config = ConfigHandle.read_config()
-    
-    # 连接WebSocket服务器
-    ws_client = WebSocketClient(bot_config.get("ws_url"))
-    
-    # 加载并注册插件
-    await ws_client.plugin_manager.load_plugins()
-    
-    await ws_client.start()
-    ws = ws_client.get_connection()
+    try:
+        # 读取配置文件
+        ConfigHandle = Config.FileConfig("config.json")
+        bot_config = ConfigHandle.read_config()
+        
+        # 连接WebSocket服务器
+        ws_client = WebSocketClient(bot_config.get("ws_url"))
+        
+        # 加载并注册插件
+        await ws_client.plugin_manager.load_plugins()
+        
+        await ws_client.start()
+        ws = ws_client.get_connection()
+    except Exception as e:
+        print(f"[ERROR] {e}")
 
 if __name__ == "__main__":
     try:
         tornado.ioloop.IOLoop.current().run_sync(main)
     except KeyboardInterrupt:
         print("[INFO] 程序已终止")
-    except Exception as e:
-        print("[ERROR] ",e)
